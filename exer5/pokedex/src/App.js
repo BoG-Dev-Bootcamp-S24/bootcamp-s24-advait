c6b3f84281578b78020e252ffa036fa1fe80cdc3
Advait Sepuri <advaitsepuri@gmail.com>
2024-02-21 17:40:01 -0500

    buttons to change pokemon

--- exer5/pokedex/src/App.js
+++ exer5/pokedex/src/App.js
@@ -28,9 +28,15 @@ function App() {
   };
 
   // Function to handle button click and increment dexNumber
-  const handleButtonClick = () => {
+  const incrementPokemon = () => {
     setDexNumber(prevDexNumber => prevDexNumber + 1);
   };
+
+  const decrementPokemon = () => {
+    if (dexNumber !== 0) {
+      setDexNumber(prevDexNumber => prevDexNumber - 1);
+    }
+  }
 
   // Fetch initial Pokemon data when component mounts
   useEffect(() => {
@@ -48,13 +54,19 @@ function App() {
         <h1 className="pl-90 text-3xl font-bold">Exercise 5: PokeDex</h1>
       </div>
       <div className="flex mb-4">
-        <div className="w-1/2 bg-gray-400 h-15">
-          <div className="top-0 left-0 w-full h-5 bg-red-300"></div>
-          <div className="top-4 left-0 w-full h-5 bg-green-300"></div>
-          <div className="top-8 left-0 w-full h-5 bg-blue-300"></div>
+        <div className="w-1/2 bg-gray-400 h-60">
+          <div className="top-0 left-0 w-full h-20 bg-red-300"></div>
+          <div className="top-4 left-0 w-full h-20 bg-green-300"></div>
+          <div className="top-8 left-0 w-full h-20 bg-white flex justify-center">
+            <button onClick={decrementPokemon} className="bg-gray-200 text-black px-4 py-1 rounded-md font-semibold hover:bg-gray-500 focus:ring-4 focus:ring-green-400 focus:outline-none transition-colors duration-300">
+              &lt;
+            </button>
+            <button onClick={incrementPokemon} className="bg-gray-200 text-black px-4 py-1 rounded-md font-semibold hover:bg-gray-500 focus:ring-4 focus:ring-green-400 focus:outline-none transition-colors duration-300">
+              &gt;
+            </button>
+          </div>
         </div>
-        <div className="w-1/2 bg-gray-500 h-50">
-          <button onClick={handleButtonClick}>Next Pokemon</button>
+        <div className="w-1/2 bg-gray-500 h-60">
           {pokemonJSON && (
             <div>
               <h2>{pokemonJSON.name}</h2>
